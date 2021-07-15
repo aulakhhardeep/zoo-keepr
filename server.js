d43bec692f783ad1fda2c6c07f7d2de495e5404f
Hardeep <aulakh.hardeep91@gmail.com>
2021-07-15 00:05:33 -0400

    added routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -110,6 +111,25 @@ app.post('/api/animals', (req, res) => {
   }
 });
 
+//routes for HTML pages
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.listen(3001, () => {
     console.log(`API server now on port ${PORT}!`);
   });
+
+  
